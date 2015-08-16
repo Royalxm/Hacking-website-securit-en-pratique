fefba2bd25ec24ec360617b979ddbc9d8746b14c
Royalxm <royalxm@hotmail.fr>
2015-08-16 19:41:46 +0200

    tutoriel 2
    tutoriel 2

---
+++ 2/php/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$login = 'admin';
+$password = 'r00t';
+
+?>
+
+<html>
+<head>
+	<title>Tuto2</title>
+</head>
+<body>
+	<?php
+	
+	if (isset($_POST['log']))
+	{
+		if ($_POST['login'] == $login && $_POST['password'] == $password)
+		{
+			echo 'Connection accepter';
+		}
+		else
+		{
+			echo 'Error';
+		}
+	}
+	else
+	{
+	echo "
+		<form method=\"POST\">
+			<input type=\"text\" name=\"login\" /> Login <br />
+			<input type=\"password\" name=\"password\" /> Password <br />
+			<input type=\"submit\" name=\"log\" value=\"Login\" />
+		</form>
+	";
+	}
+	
+</body>
